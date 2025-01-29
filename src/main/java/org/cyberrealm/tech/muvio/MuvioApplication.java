5fe901a54460fc3c343b21c83f71e7500fa36beb
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-01-29 15:31:17 +0200

    Initial commit

---
+++ src/main/java/org/cyberrealm/tech/muvio/MuvioApplication.java
@@ -0,0 +1,13 @@
+package org.cyberrealm.tech.muvio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuvioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuvioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cyberrealm/tech/muvio/MuvioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cyberrealm.tech.muvio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuvioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
