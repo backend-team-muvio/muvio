c331ce2e247fbd431a54abb8c6cb53a88691bc07
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-05 00:59:48 +0200

    Added method toEntity. Refactored toActorDto. Created toDuration method

--- src/main/java/org/cyberrealm/tech/muvio/mapper/MovieMapper.java
+++ src/main/java/org/cyberrealm/tech/muvio/mapper/MovieMapper.java
@@ -1,6 +1,8 @@
 package org.cyberrealm.tech.muvio.mapper;
 
+import info.movito.themoviedbapi.model.movies.MovieDb;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import org.cyberrealm.tech.muvio.config.MapperConfig;
 import org.cyberrealm.tech.muvio.dto.ActorDto;
@@ -8,23 +10,22 @@ import org.cyberrealm.tech.muvio.dto.MovieDto;
 import org.cyberrealm.tech.muvio.model.Actor;
 import org.cyberrealm.tech.muvio.model.GenreEntity;
 import org.cyberrealm.tech.muvio.model.Movie;
-import org.cyberrealm.tech.muvio.model.Photo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class)
 public interface MovieMapper {
-    @Mapping(source = "rating.rating", target = "rating")
-    @Mapping(source = "duration.duration", target = "duration")
-    @Mapping(source = "producer.name", target = "producer")
-    @Mapping(source = "photos", target = "photos", qualifiedByName = "toPhotosDto")
     @Mapping(source = "actors", target = "actorsDto", qualifiedByName = "toActorDto")
     @Mapping(source = "reviews", target = "reviewsDto")
-    @Mapping(source = "releaseYear.year", target = "releaseYear")
     @Mapping(source = "genres", target = "genresDto", qualifiedByName = "toGenresDto")
-    @Mapping(source = "posterPath.path", target = "posterPath")
+    @Mapping(source = "duration", target = "duration", qualifiedByName = "toDuration")
     MovieDto toMovieDto(Movie movie);
+
+    @Mapping(target = "id", expression = "java(String.valueOf(movieDb.getId()))")
+    @Mapping(source = "movieDb.runtime", target = "duration")
+    @Mapping(source = "movieDb.voteAverage", target = "rating")
+    Movie toEntity(MovieDb movieDb);
 
     @Named("toGenresDto")
     default Set<String> toGenresDto(Set<GenreEntity> genres) {
@@ -34,13 +35,16 @@ public interface MovieMapper {
     }
 
     @Named("toActorDto")
-    default Set<ActorDto> toActorDto(Set<Actor> actor) {
-        return actor.stream()
-                .map(a -> new ActorDto(a.getName(), a.getPhoto())).collect(Collectors.toSet());
+    default Set<ActorDto> toActorDto(Set<Actor> actors) {
+        return actors.stream()
+                .map(actor -> new ActorDto(actor.getName(), actor.getPhoto()))
+                .collect(Collectors.toSet());
     }
 
-    @Named("toPhotosDto")
-    default Set<String> toPhotosDto(Set<Photo> photos) {
-        return photos.stream().map(Photo::getPath).collect(Collectors.toSet());
+    @Named("toDuration")
+    default String toDuration(Integer duration) {
+        long hours = TimeUnit.MINUTES.toHours(duration);
+        long minutes = duration - TimeUnit.HOURS.toMinutes(hours);
+        return String.format("%dh %02dm", hours, minutes);
     }
 }
