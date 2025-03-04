20ca6f717a2dc503a20d296075704e3ef86a50e5
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-05 00:57:33 +0200

    Renamed variables, removed @DBRef and entities

--- src/main/java/org/cyberrealm/tech/muvio/model/Movie.java
+++ src/main/java/org/cyberrealm/tech/muvio/model/Movie.java
@@ -4,36 +4,31 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
+@Getter
+@Setter
 @Document(collection = "movies")
 public class Movie {
     @Id
     private String id;
-    private String name;
+    private String title;
     @DBRef
     private Set<GenreEntity> genres = new HashSet<>();
-    @DBRef
-    private Rating rating;
+    private Double rating;
     private String trailer;
-    @DBRef
-    private Photo posterPath;
-    @DBRef
-    private Duration duration;
-    @DBRef
-    private Producer producer;
-    @DBRef
-    private Set<Photo> photos = new HashSet<>();
+    private String posterPath;
+    private Integer duration;
+    private String producer;
+    private Set<String> photos = new HashSet<>();
     @DBRef
     private Set<Actor> actors = new HashSet<>();
-    @DBRef
     private List<Review> reviews = new ArrayList<>();
-    @DBRef
-    private Year releaseYear;
+    private Integer releaseYear;
     private String overview;
     private Set<Vibe> vibes = new HashSet<>();
     private Set<Category> categories = new HashSet<>();
