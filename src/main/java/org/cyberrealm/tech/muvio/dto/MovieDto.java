df5c9a22cd2d702ff49c285e44727de7bc474158
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-13 11:08:57 +0200

    Renamed variable to director

--- src/main/java/org/cyberrealm/tech/muvio/dto/MovieDto.java
+++ src/main/java/org/cyberrealm/tech/muvio/dto/MovieDto.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public record MovieDto(
         String id, String title, Set<String> genresDto, double rating, String trailer,
-        String posterPath, String duration, String producer, Set<String> photos,
+        String posterPath, String duration, String director, Set<String> photos,
         Set<ActorDto> actorsDto, List<ReviewDto> reviewsDto, int releaseYear,
         String overview, Set<String> keywords) {
 }
