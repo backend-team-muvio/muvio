5d033611228fd6a12af054782d1140ad06b719cd
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-13 11:10:10 +0200

    Added pageable

--- src/main/java/org/cyberrealm/tech/muvio/service/MovieService.java
+++ src/main/java/org/cyberrealm/tech/muvio/service/MovieService.java
@@ -3,10 +3,11 @@ package org.cyberrealm.tech.muvio.service;
 import java.util.List;
 import org.cyberrealm.tech.muvio.dto.MovieDto;
 import org.cyberrealm.tech.muvio.model.Movie;
+import org.springframework.data.domain.Pageable;
 
 public interface MovieService {
 
-    List<MovieDto> getAllMovies();
+    List<MovieDto> getAllMovies(Pageable pageable);
 
     Movie getMovieById(String id);
 
