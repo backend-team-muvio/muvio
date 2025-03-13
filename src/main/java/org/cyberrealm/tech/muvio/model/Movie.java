5e64e1872f515568c4a3ae76e25722bdedc4a367
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-13 11:06:17 +0200

    Reorder fields for better understanding

--- src/main/java/org/cyberrealm/tech/muvio/model/Movie.java
+++ src/main/java/org/cyberrealm/tech/muvio/model/Movie.java
@@ -6,7 +6,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
@@ -19,19 +22,22 @@ public class Movie {
     @Id
     private String id;
     private String title;
-    private Set<GenreEntity> genres = new HashSet<>();
-    private Double rating;
-    private String trailer;
+    private String overview;
+    private Integer releaseYear;
     private String posterPath;
+    private String trailer;
+    private Double rating;
     private Integer duration;
     private String director;
+    private Type type;
+
+    private Set<GenreEntity> genres = new HashSet<>();
     private Set<String> photos = new HashSet<>();
+
     @DBRef
     private Map<String, Actor> actors = new HashMap<>();
     private List<Review> reviews = new ArrayList<>();
-    private Integer releaseYear;
-    private String overview;
     private Set<Vibe> vibes = new HashSet<>();
     private Set<Category> categories = new HashSet<>();
-    private Type type;
+
 }
