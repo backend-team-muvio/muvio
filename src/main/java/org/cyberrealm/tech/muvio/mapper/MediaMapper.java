d5566d1b8fa17a00a1d39d420835742a4b4ce960
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-18 11:32:55 +0200

    Renamed MovieMapper to MediaMapper

--- src/main/java/org/cyberrealm/tech/muvio/mapper/MediaMapper.java
+++ src/main/java/org/cyberrealm/tech/muvio/mapper/MediaMapper.java
@@ -3,25 +3,25 @@ package org.cyberrealm.tech.muvio.mapper;
 import info.movito.themoviedbapi.model.movies.MovieDb;
 import java.util.concurrent.TimeUnit;
 import org.cyberrealm.tech.muvio.config.MapperConfig;
-import org.cyberrealm.tech.muvio.dto.MovieDto;
+import org.cyberrealm.tech.muvio.dto.MediaDto;
 import org.cyberrealm.tech.muvio.dto.MovieDtoFromDb;
 import org.cyberrealm.tech.muvio.dto.MovieDtoWithCast;
 import org.cyberrealm.tech.muvio.dto.MovieDtoWithCastFromDb;
-import org.cyberrealm.tech.muvio.model.Movie;
+import org.cyberrealm.tech.muvio.model.Media;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class, uses = {ActorMapper.class, GenreMapper.class})
-public interface MovieMapper {
+public interface MediaMapper {
     @Mapping(source = "actors", target = "actors", qualifiedByName = "toActorDto")
     @Mapping(source = "duration", target = "duration", qualifiedByName = "toDuration")
-    MovieDto toMovieDto(MovieDtoFromDb movie);
+    MediaDto toMovieDto(MovieDtoFromDb movie);
 
     @Mapping(target = "id", expression = "java(String.valueOf(movieDb.getId()))")
     @Mapping(source = "movieDb.runtime", target = "duration")
     @Mapping(source = "movieDb.voteAverage", target = "rating")
-    Movie toEntity(MovieDb movieDb);
+    Media toEntity(MovieDb movieDb);
 
     @Mapping(source = "actors", target = "actors", qualifiedByName = "toSetActors")
     @Mapping(source = "duration", target = "duration", qualifiedByName = "toDuration")
