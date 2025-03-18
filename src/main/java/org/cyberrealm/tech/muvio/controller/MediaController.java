cb4f7ad78012411cb090d0aa19b5691d8e599c67
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-18 11:31:48 +0200

    Renamed Movie to Media

--- src/main/java/org/cyberrealm/tech/muvio/controller/MediaController.java
+++ src/main/java/org/cyberrealm/tech/muvio/controller/MediaController.java
@@ -3,16 +3,16 @@ package org.cyberrealm.tech.muvio.controller;
 import jakarta.validation.Valid;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.cyberrealm.tech.muvio.dto.MediaDto;
 import org.cyberrealm.tech.muvio.dto.MovieBaseDto;
 import org.cyberrealm.tech.muvio.dto.MovieBaseDtoWithPoints;
-import org.cyberrealm.tech.muvio.dto.MovieDto;
 import org.cyberrealm.tech.muvio.dto.MovieDtoWithCast;
 import org.cyberrealm.tech.muvio.dto.MovieGalleryRequestDto;
 import org.cyberrealm.tech.muvio.dto.MovieVibeRequestDto;
 import org.cyberrealm.tech.muvio.dto.PosterDto;
 import org.cyberrealm.tech.muvio.dto.TitleDto;
-import org.cyberrealm.tech.muvio.model.Movie;
-import org.cyberrealm.tech.muvio.service.MovieService;
+import org.cyberrealm.tech.muvio.model.Media;
+import org.cyberrealm.tech.muvio.service.MediaService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -25,71 +25,71 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("/media")
 @RequiredArgsConstructor
-public class MovieController {
-    private final MovieService movieService;
+public class MediaController {
+    private final MediaService mediaService;
 
     @GetMapping("/{id}")
-    public MovieDto getMovieById(@PathVariable String id) {
-        return movieService.getMovieById(id);
+    public MediaDto getMovieById(@PathVariable String id) {
+        return mediaService.getMovieById(id);
     }
 
     @PostMapping
-    public Movie saveMovie(@RequestBody Movie movie) {
-        return movieService.saveMovie(movie);
+    public Media saveMovie(@RequestBody Media media) {
+        return mediaService.saveMovie(media);
     }
 
     @DeleteMapping("/{id}")
     public void deleteMovieById(@PathVariable String id) {
-        movieService.deleteMovieById(id);
+        mediaService.deleteMovieById(id);
     }
 
     @PutMapping("/{id}")
-    public Movie updateMovie(@PathVariable String id, @RequestBody Movie movie) {
-        return movieService.updateMovie(id, movie);
+    public Media updateMovie(@PathVariable String id, @RequestBody Media media) {
+        return mediaService.updateMovie(id, media);
     }
 
     @GetMapping("/vibe")
     public Slice<MovieBaseDtoWithPoints> getAllMoviesByVibe(
             @RequestBody @Valid MovieVibeRequestDto requestDto, Pageable pageable) {
-        return movieService.getAllMoviesByVibe(requestDto, pageable);
+        return mediaService.getAllMoviesByVibe(requestDto, pageable);
     }
 
     @GetMapping("/gallery")
     public Slice<MovieBaseDto> getAllForGallery(@RequestBody MovieGalleryRequestDto requestDto,
                                                 Pageable pageable) {
-        return movieService.getAllForGallery(requestDto, pageable);
+        return mediaService.getAllForGallery(requestDto, pageable);
     }
 
     @GetMapping("/luck/{size}")
     public Set<MovieBaseDto> getAllLuck(@PathVariable int size) {
-        return movieService.getAllLuck(size);
+        return mediaService.getAllLuck(size);
     }
 
     @GetMapping("/recommendations")
     public Slice<MovieBaseDto> getRecommendations(Pageable pageable) {
-        return movieService.getRecommendations(pageable);
+        return mediaService.getRecommendations(pageable);
     }
 
     @GetMapping("/top-list/{topList}")
     public Slice<MovieDtoWithCast> getMoviesByTopList(@PathVariable String topList,
                                                       Pageable pageable) {
-        return movieService.findMoviesByTopLists(topList, pageable);
+        return mediaService.findMoviesByTopLists(topList, pageable);
     }
 
     @GetMapping("/posters")
     public Slice<PosterDto> findAllPosters(Pageable pageable) {
-        return movieService.findAllPosters(pageable);
+        return mediaService.findAllPosters(pageable);
     }
 
     @GetMapping("/titles")
     public Slice<TitleDto> findAllTitle(Pageable pageable) {
-        return movieService.findAllTitles(pageable);
+        return mediaService.findAllTitles(pageable);
     }
 
     @GetMapping("/titles/{title}")
-    public MovieDto findByTitle(@PathVariable String title) {
-        return movieService.findByTitle(title);
+    public MediaDto findByTitle(@PathVariable String title) {
+        return mediaService.findByTitle(title);
     }
 }
