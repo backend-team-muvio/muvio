be720dab0c1094ac0abcabf4dfccb0ac4ce51e06
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-18 11:33:57 +0200

    Renamed MovieService to MediaService

--- src/main/java/org/cyberrealm/tech/muvio/service/MediaService.java
+++ src/main/java/org/cyberrealm/tech/muvio/service/MediaService.java
@@ -1,19 +1,42 @@
 package org.cyberrealm.tech.muvio.service;
 
-import java.util.List;
+import java.util.Set;
 import org.cyberrealm.tech.muvio.dto.MediaDto;
+import org.cyberrealm.tech.muvio.dto.MovieBaseDto;
+import org.cyberrealm.tech.muvio.dto.MovieBaseDtoWithPoints;
+import org.cyberrealm.tech.muvio.dto.MovieDtoWithCast;
+import org.cyberrealm.tech.muvio.dto.MovieGalleryRequestDto;
+import org.cyberrealm.tech.muvio.dto.MovieVibeRequestDto;
+import org.cyberrealm.tech.muvio.dto.PosterDto;
+import org.cyberrealm.tech.muvio.dto.TitleDto;
 import org.cyberrealm.tech.muvio.model.Media;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface MediaService {
 
-    List<MediaDto> getAllMovies(Pageable pageable);
-
-    Media getMovieById(String id);
+    MediaDto getMovieById(String id);
 
     Media saveMovie(Media media);
 
     void deleteMovieById(String id);
 
     Media updateMovie(String id, Media updatedMedia);
+
+    Slice<MovieBaseDtoWithPoints> getAllMoviesByVibe(MovieVibeRequestDto requestDto,
+                                                     Pageable pageable);
+
+    Slice<MovieBaseDto> getAllForGallery(MovieGalleryRequestDto requestDto, Pageable pageable);
+
+    Set<MovieBaseDto> getAllLuck(int size);
+
+    Slice<MovieBaseDto> getRecommendations(Pageable pageable);
+
+    Slice<MovieDtoWithCast> findMoviesByTopLists(String topList, Pageable pageable);
+
+    Slice<PosterDto> findAllPosters(Pageable pageable);
+
+    Slice<TitleDto> findAllTitles(Pageable pageable);
+
+    MediaDto findByTitle(String title);
 }
