afc2a5f5ed9355de6c6d3513ad37f84ba0464918
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-03-19 18:39:07 +0200

    Added toEntity for TvSerials

--- src/main/java/org/cyberrealm/tech/muvio/mapper/MediaMapper.java
+++ src/main/java/org/cyberrealm/tech/muvio/mapper/MediaMapper.java
@@ -1,6 +1,7 @@
 package org.cyberrealm.tech.muvio.mapper;
 
 import info.movito.themoviedbapi.model.movies.MovieDb;
+import info.movito.themoviedbapi.model.tv.series.TvSeriesDb;
 import java.util.concurrent.TimeUnit;
 import org.cyberrealm.tech.muvio.config.MapperConfig;
 import org.cyberrealm.tech.muvio.dto.MediaDto;
@@ -22,6 +23,13 @@ public interface MediaMapper {
     @Mapping(source = "movieDb.runtime", target = "duration")
     @Mapping(source = "movieDb.voteAverage", target = "rating")
     Media toEntity(MovieDb movieDb);
+
+    @Mapping(source = "tvSeriesDb.name", target = "title")
+    @Mapping(source = "tvSeriesDb.lastEpisodeToAir.runtime", target = "duration")
+    @Mapping(source = "tvSeriesDb.voteAverage", target = "rating")
+    @Mapping(source = "genres", target = "genres", ignore = true)
+    @Mapping(source = "type", target = "type", ignore = true)
+    Media toEntity(TvSeriesDb tvSeriesDb);
 
     @Mapping(source = "actors", target = "actors", qualifiedByName = "toSetActors")
     @Mapping(source = "duration", target = "duration", qualifiedByName = "toDuration")
