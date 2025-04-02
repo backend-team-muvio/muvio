f9b0217c66f34a6acad5e10b6aaebcb1926af05f
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-04-02 15:23:48 +0300

    Modified query according to updates in TitleDto

--- src/main/java/org/cyberrealm/tech/muvio/repository/media/MediaRepository.java
+++ src/main/java/org/cyberrealm/tech/muvio/repository/media/MediaRepository.java
@@ -51,7 +51,7 @@ public interface MediaRepository extends MongoRepository<Media, String> {
     @Query(value = "{ 'posterPath': { '$ne': null } }", fields = "{ 'id': 1, 'posterPath': 1 }")
     Slice<PosterDto> findAllPosters(Pageable pageable);
 
-    @Query(value = "{}", fields = "{ 'title': 1 }")
+    @Query(value = "{}", fields = "{ 'id': 1, 'title': 1 }")
     Slice<TitleDto> findAllTitles(Pageable pageable);
 
     MediaDtoFromDb findByTitle(String title);
